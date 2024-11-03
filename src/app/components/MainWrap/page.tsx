1bde9f6074e081092e7d4d119a4da3b2e24c712b
MOLOKO220 <93677804+MOLOKO220@users.noreply.github.com>
2024-11-03 22:45:01 +0200

    Update page.tsx

--- src/app/components/MainWrap/page.tsx
+++ src/app/components/MainWrap/page.tsx
@@ -9,13 +9,15 @@ export default function MainWrap() {
   const [currentProductIndex, setCurrentProductIndex] = useState<number>(0);
   const currentProduct = products[currentProductIndex];
 
+  const text1 = JSON.stringify(currentProduct.images);
+
   function switchProduct(index: number) {
     setCurrentProductIndex(index);
   }
 
   return (
     <main className="MainWrap container">
-      <ImageDisplay images={currentProduct.images} />
+      <ImageDisplay images={text1} />
       <ProductInfo product={currentProduct} onSwitchProduct={switchProduct} />
     </main>
   );
